cddb436f61fe99a9c136b4bc384726159310de1f
Petru Trimbitas <petru.trimbitas@yahoo.com>
2018-09-10 15:40:12 +0300

    Add email text and redirect

--- MailingList.pl
+++ MailingList.pl
@@ -15,21 +15,39 @@ post '/add-email' => sub {
     return;
   }
   $pg->db->insert('emails', {email => $email, date_submitted => gmstamp(time)});
-  $c->render(text => "Successfully registered");
+  my $email_text =
+<<EMAIL_END
+Hi there,
+
+Thanks for subscribing to our newsletter, we promise we'll only send you
+content that we believe is relevant to you. Keep an eye on our facebook page
+https://facebook.com/wellcode.ro/ and on https://wellcode.com since we'll soon
+be launching our starter course in computer programming!
+
+In the meantime, I hope you enjoy those two books I personally prepared to help
+you get a clear picture of your path to being a successful software engineer.
+They're both attached here.
+
+Hope to see you soon on wellcode.com!
+Petru,
+Co-founder of Wellcode
+EMAIL_END
+
   my $domain = $ENV{MAILGUN_DOMAIN};
   my $api_key = $ENV{MAILGUN_API_KEY};
   my $command = "curl -s --user 'api:$api_key' " .
     "https://api.mailgun.net/v3/$domain/messages " .
     "-F from='Excited User <mailgun\@$domain>' " .
     "-F to=$email " .
-    "-F subject='Hello' " .
-    "-F text='Testing some Mailgun awesomeness!' ";
+    "-F subject='Here are your books for your WellCode subscription' ";
+    "-F text='$email_text' ";
 
   my @files = split ',', $ENV{MAILGUN_FILES};
   for my $file (@files) {
     $command .= "-F attachment=\@$file ";
   }
   system $command;
+  $c->redirect_to($ENV{MAILGUN_REDIRECT_TO});
 };
 
 app->start;
