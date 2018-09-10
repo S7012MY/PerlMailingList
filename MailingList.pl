d15aa29a0ecb785e053759a879ae4c85360fcaf5
Petru Trimbitas <petru.trimbitas@yahoo.com>
2018-09-10 15:42:18 +0300

    Fix previous commit

--- MailingList.pl
+++ MailingList.pl
@@ -8,25 +8,22 @@ my $pg = Mojo::Pg->new('postgresql:///mailing_list');
 post '/add-email' => sub {
   my $c = shift;
   my $email = $c->param('email');
-  use v5.24;
-  say $email;
   if (!Email::Valid->address(-address => $email, -mxcheck => 1)) {
     $c->render(text => "Email address is invalid");
     return;
   }
   $pg->db->insert('emails', {email => $email, date_submitted => gmstamp(time)});
-  my $email_text =
-<<EMAIL_END
+  my $email_text = <<EMAIL_END;
 Hi there,
 
-Thanks for subscribing to our newsletter, we promise we'll only send you
+Thanks for subscribing to our newsletter, we promise we will only send you
 content that we believe is relevant to you. Keep an eye on our facebook page
-https://facebook.com/wellcode.ro/ and on https://wellcode.com since we'll soon
+https://facebook.com/wellcode.ro/ and on https://wellcode.com since we will soon
 be launching our starter course in computer programming!
 
 In the meantime, I hope you enjoy those two books I personally prepared to help
 you get a clear picture of your path to being a successful software engineer.
-They're both attached here.
+I have attached them here.
 
 Hope to see you soon on wellcode.com!
 Petru,
@@ -37,10 +34,10 @@ EMAIL_END
   my $api_key = $ENV{MAILGUN_API_KEY};
   my $command = "curl -s --user 'api:$api_key' " .
     "https://api.mailgun.net/v3/$domain/messages " .
-    "-F from='Excited User <mailgun\@$domain>' " .
+    "-F from='Petru Trimbitas <mailgun\@$domain>' " .
     "-F to=$email " .
-    "-F subject='Here are your books for your WellCode subscription' ";
-    "-F text='$email_text' ";
+    "-F subject='Here are your books for your WellCode subscription' " .
+    "-F text='" . $email_text . "' ";
 
   my @files = split ',', $ENV{MAILGUN_FILES};
   for my $file (@files) {
